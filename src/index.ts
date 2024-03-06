4be4526d1002db961b4d668d549fd824ec854f31
dapotatoman <iam.fahadul@gmail.com>
2024-03-06 13:38:43 +0600

    feat: Add JSX

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { Plugin } from 'vite'
 
+import VueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools, { type VitePluginVueDevToolsOptions } from 'vite-plugin-vue-devtools'
 import TurboConsole from 'unplugin-turbo-console/vite'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -16,6 +17,9 @@ export interface Config {
   /** {@link https://github.com/unplugin/unplugin-turbo-console unplugin-turbo-console} */
   console: import('unplugin-turbo-console/types').Options
 
+  /** {@link https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue-jsx @vitejs/plugin-vue-jsx} */
+  jsx: import('@vitejs/plugin-vue-jsx').Options | false
+
   /** {@link https://github.com/unplugin/unplugin-auto-import unplugin-auto-import} */
   autoimport: import('unplugin-auto-import/types').Options | false
 
@@ -27,6 +31,7 @@ export default (config: Partial<Config>): Plugin[] => {
   return [
     VueDevTools(config.devtools),
     TurboConsole(config.console),
+    applyPlugin(VueJsx, config.jsx, undefined, true),
     applyPlugin(AutoImport, config.autoimport, undefined, true),
     applyPlugin(Components, config.components, undefined, true),
   ]
