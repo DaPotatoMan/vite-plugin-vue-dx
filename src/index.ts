d9e5ddbebe2ed25015764fabf78cef230f1ea9c6
dapotatoman <iam.fahadul@gmail.com>
2024-03-28 17:55:09 +0600

    feat: Make all plugins opt-in by default

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,23 @@ import { components, console, devtools, fontaine, imports, jsx } from './exports
 const plugins = { components, console, devtools, fontaine, imports, jsx }
 
 type PluginMap = typeof plugins
+type PluginOption<T> = T extends undefined ? T | boolean : T | false
 
 export type VueDXConfig = Partial<{
-  [K in keyof PluginMap]: Parameters<PluginMap[K]>[0] | false
+  [K in keyof PluginMap]: PluginOption<Parameters<PluginMap[K]>[0]>
 }>
 
 export function VueDX(config: VueDXConfig = {}) {
   const list: any[] = []
 
   for (const [key, plugin] of Object.entries(plugins)) {
-    const pluginArgs = config[key as keyof PluginMap]
+    const args: any = config[key as keyof PluginMap]
 
-    // Skip disabled plugin
-    if (pluginArgs === false)
-      continue
-
-    list.push(
-      plugin(pluginArgs as any),
-    )
+    if (args) {
+      list.push(
+        plugin(args === true ? undefined : args),
+      )
+    }
   }
 
   return list
