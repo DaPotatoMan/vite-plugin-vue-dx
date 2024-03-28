da036156132cda2e1ea228db4e358fa35f4ea5f5
dapotatoman <iam.fahadul@gmail.com>
2024-03-28 16:48:23 +0600

    feat: Improve implementation

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
+import type { Plugin } from 'vite'
 import * as DX from './exports'
-import { type Plugin, applyPlugin } from './utils'
 
 export * from './exports'
 
-export interface Config {
-  /** {@link https://devtools-next.vuejs.org/ devtools-next} */
-  devtools: import('vite-plugin-vue-devtools').VitePluginVueDevToolsOptions
+const plugins = {
+  devtools: DX.VueDevTools,
+  console: DX.TurboConsole,
 
-  /** {@link https://github.com/unplugin/unplugin-turbo-console unplugin-turbo-console} */
-  console: import('unplugin-turbo-console/types').Options
-
-  /** {@link https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue-jsx @vitejs/plugin-vue-jsx} */
-  jsx: import('@vitejs/plugin-vue-jsx').Options | false
-
-  /** {@link https://github.com/unplugin/unplugin-auto-import unplugin-auto-import} */
-  autoimport: import('unplugin-auto-import/types').Options | false
-
-  /** {@link https://github.com/unplugin/unplugin-vue-components unplugin-vue-components} */
-  components: import('unplugin-vue-components/types').Options
+  jsx: DX.VueJSX,
+  imports: DX.Imports,
+  components: DX.Components,
 }
 
-export default (config: Partial<Config>): Plugin[] => {
-  return [
-    DX.VueDevTools(config.devtools),
-    DX.TurboConsole(config.console),
-    applyPlugin(DX.VueJSX, config.jsx, undefined, true),
-    applyPlugin(DX.Imports, config.autoimport, undefined, true),
-    applyPlugin(DX.Components, config.components, undefined, true),
-  ]
-    .flatMap(e => e)
+type PluginMap = typeof plugins
+
+type Config = {
+  [K in keyof PluginMap]: Parameters<PluginMap[K]>[0] | false
+}
+
+export function VueDX(config: Partial<Config>): Plugin[] {
+  const list: any[] = []
+
+  for (const [key, plugin] of Object.entries(plugins)) {
+    const pluginArgs = config[key as keyof PluginMap]
+
+    // Skip disabled plugin
+    if (pluginArgs === false)
+      continue
+
+    list.push(
+      plugin(pluginArgs as any),
+    )
+  }
+
+  return list
 }
