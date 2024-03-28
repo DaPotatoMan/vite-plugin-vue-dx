9f25fedb0f4c5d2994aedd2e39975b4c49ea897f
dapotatoman <iam.fahadul@gmail.com>
2024-03-28 16:55:06 +0600

    chore: add docs for exports

--- src/exports.ts
+++ src/exports.ts
@@ -1,8 +1,26 @@
-export { default as jsx } from '@vitejs/plugin-vue-jsx'
-export { default as devtools } from 'vite-plugin-vue-devtools'
-export { default as console } from 'unplugin-turbo-console/vite'
+export {
+  /** Vue JSX using {@link https://github.com/vitejs/vite-plugin-vue/tree/main/packages/plugin-vue-jsx @vitejs/plugin-vue-jsx} */
+  default as jsx,
+} from '@vitejs/plugin-vue-jsx'
 
-export { /** Auto import components */ default as components } from 'unplugin-vue-components/vite'
+export {
+  /** Vue {@link https://devtools-next.vuejs.org/ devtools} */
+  default as devtools,
+} from 'vite-plugin-vue-devtools'
+
+export {
+  /** Supercharge your console using {@link https://github.com/unplugin/unplugin-turbo-console unplugin-turbo-console} */
+  default as console,
+} from 'unplugin-turbo-console/vite'
+
+export {
+  /** Auto import components using {@link https://github.com/unplugin/unplugin-vue-components unplugin-vue-components} */
+  default as components,
+} from 'unplugin-vue-components/vite'
+
 export * from 'unplugin-vue-components/resolvers'
 
-export { /** Auto import */ default as imports } from 'unplugin-auto-import/vite'
+export {
+  /** Auto imports using {@link https://github.com/unplugin/unplugin-auto-import unplugin-auto-import} */
+  default as imports,
+} from 'unplugin-auto-import/vite'
