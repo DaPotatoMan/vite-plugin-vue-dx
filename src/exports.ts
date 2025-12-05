353f6d3393d1e1128d55f7b68d7c2e6476d52edc
Fahad <44726129+DaPotatoMan@users.noreply.github.com>
2025-12-06 02:29:07 +0600

    fix: lint issues

--- src/exports.ts
+++ src/exports.ts
@@ -6,26 +6,26 @@ export {
 } from '@vitejs/plugin-vue-jsx'
 
 export {
-  /** Vue {@link https://devtools-next.vuejs.org/ devtools} */
-  default as devtools,
-} from 'vite-plugin-vue-devtools'
+  /** Auto imports using {@link https://github.com/unplugin/unplugin-auto-import unplugin-auto-import} */
+  default as imports,
+} from 'unplugin-auto-import/vite'
 
 export {
   /** Supercharge your console using {@link https://github.com/unplugin/unplugin-turbo-console unplugin-turbo-console} */
   default as console,
 } from 'unplugin-turbo-console/vite'
 
+export * from 'unplugin-vue-components/resolvers'
+
 export {
   /** Auto import components using {@link https://github.com/unplugin/unplugin-vue-components unplugin-vue-components} */
   default as components,
 } from 'unplugin-vue-components/vite'
 
-export * from 'unplugin-vue-components/resolvers'
-
 export {
-  /** Auto imports using {@link https://github.com/unplugin/unplugin-auto-import unplugin-auto-import} */
-  default as imports,
-} from 'unplugin-auto-import/vite'
+  /** Vue {@link https://devtools-next.vuejs.org/ devtools} */
+  default as devtools,
+} from 'vite-plugin-vue-devtools'
 
 export type ImportPresets = Parameters<typeof import('unplugin-auto-import')['default']['vite']>[0]['imports']
 
